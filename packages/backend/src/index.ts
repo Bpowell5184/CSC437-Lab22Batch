41d83bb0937fd7a62e808a9a5155a795944cee7a
legion5184 <96603312+legion5184@users.noreply.github.com>
2025-06-10 13:21:45 -0700

    Vite request forwarding

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -10,7 +10,7 @@ const STATIC_DIR = process.env.STATIC_DIR || "public";
 const app = express();
 app.use(express.static(STATIC_DIR));
 
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/api/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
 
